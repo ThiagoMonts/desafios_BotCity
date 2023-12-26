280561d17d7303c17dab9244f9d283d3e1847119
Thiago Honorato da Silva <thiagohonorato@hotmail.com>
2023-12-26 14:00:59 -0300

    Remoção da espera

--- Desafios/Desafio 04 - Customer Onboarding - Front Office/bot.py
+++ Desafios/Desafio 04 - Customer Onboarding - Front Office/bot.py
@@ -74,42 +74,34 @@ def main():
         customer_name_field = bot.find_element("//div[contains(@class, 'form-group')]//input[@id='customerName']", By.XPATH)
         customer_name_field.send_keys(row["Company Name"])
 
-        bot.wait(500)
 
         customer_id_field = bot.find_element("//div[contains(@class, 'form-group')]//input[@id='customerID']", By.XPATH)
         customer_id_field.send_keys(row["Customer ID"])
 
-        bot.wait(500)
 
         primary_contact_field = bot.find_element("//div[contains(@class, 'form-group')]//input[@id='primaryContact']", By.XPATH)
         primary_contact_field.send_keys(row["Primary Contact"])
 
-        bot.wait(500)
 
         street_address_field = bot.find_element("//div[contains(@class, 'form-group')]//input[@id='street']", By.XPATH)
         street_address_field.send_keys(row["Street Address"])     
 
-        bot.wait(500)
 
         city_field = bot.find_element("//div[contains(@class, 'form-row')]//input[@id='city']", By.XPATH)
         city_field.send_keys(row["City"])
 
-        bot.wait(500)
 
         state_field_option = bot.find_element(f"//div[contains(@class, 'form-group')]//select[@id='state']//option [@value='{row['State']}']", By.XPATH)
         state_field_option.click()
 
-        bot.wait(500)
 
         zip_field = bot.find_element("//div[contains(@class, 'form-row')]//input[@id='zip']", By.XPATH)
         zip_field.send_keys(row["Zip"])
 
-        bot.wait(500)
 
         email_field = bot.find_element("//div[contains(@class, 'form-group')]//input[@id='email']", By.XPATH)
         email_field.send_keys(row["Email Address"])
 
-        bot.wait(500)
 
         offers_discount = row["Offers Discounts"]
         if offers_discount == "YES":
@@ -119,7 +111,6 @@ def main():
             offers_discount_field = bot.find_element("//div[contains(@class, 'form-check')]//input[@id='activeDiscountNo']", By.XPATH)
             offers_discount_field.click()
         
-        bot.wait(500)
 
         non_disclosure = row["Non-Disclosure On File"]
         non_disclosure_field = bot.find_element("//div[contains(@class, 'form-group')]//input[@id='NDA']", By.XPATH)
@@ -131,15 +122,13 @@ def main():
                 non_disclosure_field.click()
 
 
-        bot.wait(500)
 
         submit_btn = bot.find_element("//div[contains(@class, 'form-group')]//button[@id='submit_button']", By.XPATH)
         submit_btn.click()
 
-        bot.wait(500)
 
     # Wait 3 seconds before closing
-    bot.wait(3000)
+    bot.wait(1000)
 
     # Finish and clean up the Web Browser
     # You MUST invoke the stop_browser to avoid
